990a0abf600e1de78aa99353aff3ee2d3ddee323
born2code <ahmad@abualsamid.com>
2021-03-08 18:26:26 -0500

    save running updates to local storage.

--- pages/events/express.js
+++ pages/events/express.js
@@ -1,16 +1,41 @@
+import { Auth } from 'aws-amplify'
 import Layout from 'components/layout'
 import { EventTimes, TimerLabels } from 'lib/timer'
 import { useEffect, useState } from 'react'
 import { ulid } from 'ulid'
 
 function TrackTimes({ addCompletedTime }) {
-  const [timers, setTimers] = useState([])
-  console.log('express...')
+  const [timers, setTimers] = useState(window?JSON.parse(window.localStorage.getItem('coachTimer.timers') || '[]') :[])
+  const [intervalId, setIntervalId] = useState(null)
+
+  
+  const saveStateToLocalStorage = () => {
+    try {
+      if (window) {
+        window.localStorage.setItem('coachTimer.timers', JSON.stringify(timers))
+      }
+    } catch (error) {
+      console.log('error saving local storage.')
+    }
+  }
   useEffect(() => {
-    const intervalId = setInterval(updateRunningClock, 1000 * 1)
     return () => clearInterval(intervalId)
   }, [])
 
+  const stopUpdating = () => {
+    if (intervalId) {
+      console.log('stopping ticker...')
+      clearInterval(intervalId)
+      setIntervalId(null)
+    }
+  }
+  const startUpdating = () => {
+    if (!intervalId) {
+      console.log('starting ticker...')
+      const i = setInterval(updateRunningClock, 1000 * 1)
+      setIntervalId((_intervalId) => i)
+    }
+  }
   const updateRunningClock = () => {
     setTimers((timers) => {
       if (timers.length === 0) return [...timers]
@@ -21,6 +46,9 @@ function TrackTimes({ addCompletedTime }) {
         })
       return [...timers]
     })
+    if (Date.now() % 10 === 0) {
+      saveStateToLocalStorage()
+    }
   }
   const addTimer = (e) => {
     e.preventDefault()
@@ -43,11 +71,14 @@ function TrackTimes({ addCompletedTime }) {
 
     elems.athlete.value = ''
     console.log('added timer ', timer)
+    saveStateToLocalStorage()
     return false
   }
 
   const startAllTimers = () => {
     const _now = Date.now()
+    startUpdating()
+
     setTimers((timers) =>
       timers.map((timer) => {
         if (!timer.finished && !timer.started) {
@@ -57,9 +88,13 @@ function TrackTimes({ addCompletedTime }) {
         return timer
       })
     )
+    saveStateToLocalStorage()
+
   }
 
   const startTimer = (uid) => {
+    startUpdating()
+
     const d = Date.now()
     const timer = timers.find((t) => t.uid === uid)
     timer.start = d
@@ -67,6 +102,8 @@ function TrackTimes({ addCompletedTime }) {
     setTimers((timers) =>
       timers.map((_timer) => (_timer.uid === uid ? timer : _timer))
     )
+    saveStateToLocalStorage()
+
   }
   const lapTimer = (uid) => {
     const d = Date.now()
@@ -76,6 +113,8 @@ function TrackTimes({ addCompletedTime }) {
     setTimers((timers) =>
       timers.map((_timer) => (_timer.uid === uid ? timer : _timer))
     )
+    saveStateToLocalStorage()
+
   }
 
   const finishTimer = async (uid) => {
@@ -95,7 +134,25 @@ function TrackTimes({ addCompletedTime }) {
     } catch (error) {
       console.log( error)
     }
+    try {
+      if (window) {
+        window.localStorage.setItem('coachTimer.timers',JSON.stringify(timers))
+      }
+    } catch (error) {
+      console.log('error saving local storage.')
+    }
+    const remaining = timers.filter((timer) => !timer.finished).length
+    if (!remaining) {
+      stopUpdating()
+    }
+    saveStateToLocalStorage()
+
   }
+
+  if (timers.length > 0) {
+    startUpdating()
+  }
+  
   return (
       <div className="container">
         <Form addTimer={addTimer} />
@@ -149,21 +206,46 @@ function TrackTimes({ addCompletedTime }) {
   )
 }
 
-const Form = ({ addTimer }) => {
+const athletes = [
+  {name: 'Asia'},
+  {name: 'Bassel'},
+  {name: 'Ahmad'},
+  {name: 'John'},
+  {name: 'Joe'}
+]
+const matchName = (name, search) => {
+  return search.length>0 && name.includes(search)
+}
+const SearchPreview = ({name},onClick) => {
   return (
-    <form onSubmit={addTimer}>
+    <div style={{border: '1px solid black'}} onClick={e => onClick(name)}>
+      {name}
+    </div>
+  )
+}
+const Form = ({ addTimer }) => {
+  const [search, setSearch] = useState("")
+  const onClick = name => setSearch(name)
+  return (
+    <form onSubmit={addTimer} autocomplete="off">
       <div className="mb-3">
         <label htmlFor="athlete">Athlete's name</label>
-        
+
         <input
           className="form-control"
           name="athlete"
           id="athlete"
           type="text"
           placeholder="Athlete Name"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          autocomplete="off"
         ></input>
       </div>
-     
+      {athletes
+        .filter((a) => matchName(a.name, search))
+        .map((a) => SearchPreview(a, onClick))}
+
       <div className="d-grid gap-2">
         <button type="submit" className="btn btn-primary btn-lg">
           Add Athlete to Time
@@ -178,11 +260,40 @@ const Form = ({ addTimer }) => {
 
 
 const Home = () => {
-  const [completedTimes, setCompletedTimes] = useState([])
-  const addCompletedTime = (time) => setCompletedTimes((times) => [...times, time])
+  const [completedTimes, setCompletedTimes] = useState(window?JSON.parse(window.localStorage.getItem('coachTimer.completedTimes')||'[]'):[])
+  const [user, setUser] = useState(null)
+  const [athletes, setAthletes] = useState([])
+  useEffect(()=> {
+    async function checkUser() {
+      const u = await Auth.currentAuthenticatedUser()
+      console.log('current user ', u)
+      setUser(u)
+      
+    }
+    checkUser()
+  },[])
+  // if(user) {
+  //     const { data } =  useGet('/getAthletes')
+  //     if (data) {
+  //       console.log('got athlete list from server ', data)
+  //       setAthletes([...data])
+  //     }
+  // } else {
+  //   const a = window ? window.localStorage.getItem('athletes'): []
+  //   if (a) {
+  //     setAthletes([...a])
+  //   }
+  // }
+  const addCompletedTime = (time) => setCompletedTimes((times) => {
+    const newTimes = [...times, time]
+    if(window){
+      window.localStorage.setItem('coachTimer.completedTimes', JSON.stringify(newTimes))
+    }
+    return newTimes
+  })
   return (
     <Layout>
-      <TrackTimes addCompletedTime={addCompletedTime} />
+      <TrackTimes addCompletedTime={addCompletedTime} user={user} />
       <EventTimes times={completedTimes} />
     </Layout>
   )
