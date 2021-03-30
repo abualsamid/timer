41b64e9b900bd9b779076a2a62f0b9c6fbcb89e5
born2code <ahmad@abualsamid.com>
2021-03-29 20:15:13 -0400

    build qa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
 const isProd = process.env.NODE_ENV === 'production'
+
+// change start-url cache strategy, so that we can prompt user to reload when
+// new version available, instead of showing new version directly
+runtimeCaching[0].handler = 'StaleWhileRevalidate'
+
 module.exports = withPWA({
   trailingSlash: true,
   pwa: {
     disable: !isProd,
     dest: 'public',
+    register: false,
+    skipWaiting: false,
+    runtimeCaching,
   },
 })
